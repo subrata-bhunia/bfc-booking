21f35f3a7359d259e7735dcaf2a27cfb0e2c91e6
subrata-bhunia <eerrcfgy@gmail.com>
2022-04-20 22:56:51 +0530

    none

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const instance = axios.create({baseURL: 'https://debpurbfc.com/api/v1'});
-const instance = axios.create({
-  baseURL: 'https://admin.delicoapp.in/bfc/public/api/v1',
-});
+const instance = axios.create({baseURL: 'https://debpurbfc.com/api/v1'});
+// const instance = axios.create({
+//   baseURL: 'https://admin.delicoapp.in/bfc/public/api/v1',
+// });
 export default instance;
